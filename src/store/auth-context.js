2aa96ec4915b969285dc6db99bdff8332acdd297
React_Teaching_Project <priyassss789@gmail.com>
2021-12-13 16:23:47 +0530

    Persisting The User Authentication Status

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,15 +8,18 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = props => {
-    const [token, setToken] = useState(null);
+    const initialToken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialToken);
 
     const userIsLoggedIn = !!token;
 
     const loginHandler = (token) => {
         setToken(token);
+        localStorage.setItem('token', token);
     };
     const LogoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     };
 
     const contextValue = {
@@ -28,4 +31,4 @@ export const AuthContextProvider = props => {
 
     return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>;
 };
-export default AuthContext
+export default AuthContext;
